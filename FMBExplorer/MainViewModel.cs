0dc1e7dae27c8a696b12e5ca80e7730b32bcca89
horzsolt <horzsolt2006@gmail.com>
2018-06-09 17:58:32 +0200

    Add project files.

---
+++ FMBExplorer/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMBExplorer
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private string _currentFolder;
+        public string CurrentFolder
+        {
+
+            get
+            {
+                return _currentFolder;
+            }
+
+            set
+            {
+                _currentFolder = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("CurrentFolder"));
+            }
+        }
+
+        private string _fmxList;
+        public string FmxList
+        {
+
+            get
+            {
+                return _fmxList;
+            }
+
+            set
+            {
+                _fmxList = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("FmxList"));
+            }
+        }
+    }
+}
---
+++ FMBExplorer/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace FMBExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel vm = new MainViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm.CurrentFolder = System.AppDomain.CurrentDomain.BaseDirectory;
+            DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.SelectedPath = vm.CurrentFolder;
+
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                vm.CurrentFolder = dialog.SelectedPath;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles(vm.CurrentFolder, "*.xml");
+        }
+    }
+}
